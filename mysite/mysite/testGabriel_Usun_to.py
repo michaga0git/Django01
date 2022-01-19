dd2c6371783f68530cc2a06b4c031d2abfa04cca
Michalski Gabriel - Detal <Gabriel.Michalski@orange.com>
2022-01-19 12:50:49 +0100

    update2 dodanie czegos

---
+++ mysite/mysite/testGabriel_Usun_to.py
@@ -0,0 +1,18 @@
+# Tablice
+
+# tablica = []
+# tablica.append(1)
+# tablica.append(2)
+# tablica.append(3)
+# print tablica[0] # wypisze 1
+# print tablica[1] # wypisze 2
+# print tablica[2] # wypisze 3
+
+# wypisze kolejno 1, 2, 3
+# for x in tablica:
+
+###############  Tablice dodawanie ########################################
+parzyste_dodatnie = [2,4,6,8]
+nieparzyste_dodatnie = [1,3,5,7]
+naturalne = parzyste_dodatnie + nieparzyste_dodatnie
+print(naturalne)
